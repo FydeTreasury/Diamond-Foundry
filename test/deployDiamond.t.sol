ea94c686cf3f5954bf26b632ffc13a76136acd8e
Team0 <timo@fyde.fi>
2022-10-20 10:48:13 +0100

    deployDiamond fixed

--- test/deployDiamond.t.sol
+++ test/deployDiamond.t.sol
@@ -8,7 +8,11 @@ import "../src/facets/OwnershipFacet.sol";
 import "../../lib/forge-std/src/Test.sol";
 import "../src/Diamond.sol";
 
+import "solidity-stringutils/strings.sol";
+
 contract DiamondDeployer is Test, IDiamondCut {
+    using strings for *;
+
     //contract types of facets to be deployed
     Diamond diamond;
     DiamondCutFacet dCutFacet;
@@ -26,18 +30,18 @@ contract DiamondDeployer is Test, IDiamondCut {
 
         // diamod arguments
         DiamondArgs memory _args = DiamondArgs({
-            owner: address(this),
-            init: address(0),
-            initCalldata: " "
-            });
+        owner: address(this),
+        init: address(0),
+        initCalldata: " "
+        });
 
         // facets and functions to add
-        FacetCut[] memory cut0 = new FacetCut[](0);
+        FacetCut[] memory cut0 = new FacetCut[](1);
         cut0[0] = FacetCut ({
-            facetAddress: address(dCutFacet),
-            action: IDiamond.FacetCutAction.Add,
-            functionSelectors: generateSelectors("DiamondCutFacet")
-            });
+        facetAddress: address(dCutFacet),
+        action: IDiamond.FacetCutAction.Add,
+        functionSelectors: generateSelectors("DiamondCutFacet")
+        });
 
 
         // deploy diamond
@@ -71,20 +75,38 @@ contract DiamondDeployer is Test, IDiamondCut {
         DiamondLoupeFacet(address(diamond)).facetAddresses();
     }
 
+    // try implementing solidity only - still doesnt work
     function generateSelectors(string memory _facetName)
     internal
     returns (bytes4[] memory selectors)
     {
+
+        //get string of contract methods
         string[] memory cmd = new string[](4);
         cmd[0] = "forge";
         cmd[1] = "inspect";
         cmd[2] = _facetName;
         cmd[3] = "methods";
         bytes memory res = vm.ffi(cmd);
+        string memory st = string(res);
 
-        // how to convert bytes memory result to bytes4[] selector array??
-        selectors = abi.decode(res, (bytes4[]));
+        // extract function signatures and take first 4 bytes of keccak
+        strings.slice memory s = st.toSlice();
+        strings.slice memory delim = ":".toSlice();
+        strings.slice memory delim2 = ",".toSlice();
+        bytes4[] memory selectors = new bytes4[]((s.count(delim)));
+        for(uint i = 0; i < selectors.length; i++) {
+            s.split('"'.toSlice());
+            selectors[i] = bytes4(s.split(delim).until('"'.toSlice()).keccak());
+            s.split(delim2);
+
+        }
+        return selectors;
+
+
+
     }
+
 
     function diamondCut(
         FacetCut[] calldata _diamondCut,
