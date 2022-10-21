3dc7a83c5aec552122c3fb01c634f01998131d8c
Team0 <timo@fyde.fi>
2022-10-21 13:35:26 +0100

    seperate tests in different contracts with different initialisation

--- test/test1_ordered.t.sol
+++ test/test1_ordered.t.sol
@@ -12,7 +12,8 @@ import "../src/Diamond.sol";
 
 import "solidity-stringutils/strings.sol";
 
-contract DiamondDeployer is Test, IDiamondCut {
+
+contract DiamondDeployer is IDiamondCut, Test {
     using strings for *;
 
     //contract types of facets to be deployed
@@ -90,110 +91,6 @@ contract DiamondDeployer is Test, IDiamondCut {
 
     }
 
-
-    // TEST CASES
-
-
-    function test1HasThreeFacets() public {
-        assertEq(facetAddresses.length, 3);
-    }
-
-
-    function test2FacetsHaveCorrectSelectors() public {
-
-        for (uint i = 0; i < facetAddresses.length; i++) {
-            bytes4[] memory fromLoupeFacet = ILoupe.facetFunctionSelectors(facetAddresses[i]);
-            bytes4[] memory fromGenSelectors =  generateSelectors(facetNames[i]);
-            assertEq(abi.encode(fromLoupeFacet), abi.encode(fromGenSelectors));
-        }
-    }
-
-
-    function test3SelectorsAssociatedWithCorrectFacet() public {
-        for (uint i = 0; i < facetAddresses.length; i++) {
-            assertEq(facetAddresses[i], ILoupe.facetAddress(generateSelectors(facetNames[i])[0]));
-        }
-    }
-
-
-    function test4AddTest1FacetFunctions() public {
-        //deploy Test1Facet
-        Test1Facet test1Facet = new Test1Facet();
-
-        // get functions selectors but remove first element (supportsInterface)
-        bytes4[] memory fromGenSelectors = removeIndex(0, generateSelectors("Test1Facet"));
-
-        // array of functions to add
-        FacetCut[] memory cutTest1 = new FacetCut[](1);
-        cutTest1[0] =
-        FacetCut({
-        facetAddress: address(test1Facet),
-        action: FacetCutAction.Add,
-        functionSelectors: fromGenSelectors
-        });
-
-        // add functions to diamond
-        ICut.diamondCut(cutTest1, address(0x0), "");
-
-        // check if functions added to diamond
-        bytes4[] memory fromLoupeFacet = ILoupe.facetFunctionSelectors(address(test1Facet));
-        assertEq(abi.encode(fromLoupeFacet), abi.encode(fromGenSelectors));
-
-
-    }
-
-
-    function test5CanCallTest1FacetFunction() public {
-
-        //deploy Test1Facet
-        Test1Facet test1Facet = new Test1Facet();
-
-        // get functions selectors but remove first element (supportsInterface)
-        bytes4[] memory fromGenSelectors = removeIndex(0, generateSelectors("Test1Facet"));
-
-        // array of functions to add
-        FacetCut[] memory cutTest1 = new FacetCut[](1);
-        cutTest1[0] =
-        FacetCut({
-        facetAddress: address(test1Facet),
-        action: FacetCutAction.Add,
-        functionSelectors: fromGenSelectors
-        });
-
-        // add functions to diamond
-        ICut.diamondCut(cutTest1, address(0x0), "");
-
-        // try to call function on new Facet
-        Test1Facet(address(diamond)).test1Func10();
-    }
-
-    function test6ReplaceSupportsInterfaceFunction() public {
-
-        //deploy Test1Facet
-        Test1Facet test1Facet = new Test1Facet();
-
-        // get supportsInterface selector from positon 0
-        bytes4[] memory fromGenSelectors =  new bytes4[](1);
-        fromGenSelectors[0] = generateSelectors("Test1Facet")[0];
-
-        // struct to replace function
-        FacetCut[] memory cutTest1 = new FacetCut[](1);
-        cutTest1[0] =
-        FacetCut({
-        facetAddress: address(test1Facet),
-        action: FacetCutAction.Replace,
-        functionSelectors: fromGenSelectors
-        });
-
-        // replace function by function on Test1 facet
-        ICut.diamondCut(cutTest1, address(0x0), "");
-
-        // try to call function on new Facet
-        assertEq(address(test1Facet), ILoupe.facetAddress(fromGenSelectors[0]));
-
-    }
-
-
     // HELPER FUNCTIONS
 
     // return array of function selectors for given facet name
@@ -228,6 +125,7 @@ contract DiamondDeployer is Test, IDiamondCut {
 
     }
 
+    // helper to remove index from bytes4[] array
     function removeIndex(uint index, bytes4[] memory array) public pure returns (bytes4[] memory){
         for(uint i = index; i < array.length-1; i++){
             array[i] = array[i+1];
@@ -242,4 +140,101 @@ contract DiamondDeployer is Test, IDiamondCut {
         address _init,
         bytes calldata _calldata
     ) external override {}
+
+}
+
+// test proper deployment of diamond
+contract TestDeployment is DiamondDeployer {
+
+
+    // TEST CASES
+
+    function test1HasThreeFacets() public {
+        assertEq(facetAddresses.length, 3);
+    }
+
+
+    function test2FacetsHaveCorrectSelectors() public {
+
+        for (uint i = 0; i < facetAddresses.length; i++) {
+            bytes4[] memory fromLoupeFacet = ILoupe.facetFunctionSelectors(facetAddresses[i]);
+            bytes4[] memory fromGenSelectors =  generateSelectors(facetNames[i]);
+            assertEq(abi.encode(fromLoupeFacet), abi.encode(fromGenSelectors));
+        }
+    }
+
+
+    function test3SelectorsAssociatedWithCorrectFacet() public {
+        for (uint i = 0; i < facetAddresses.length; i++) {
+            assertEq(facetAddresses[i], ILoupe.facetAddress(generateSelectors(facetNames[i])[0]));
+        }
+    }
+
+}
+
+// tests proper upgrade of diamond when adding a facet
+contract TestUpgrade is DiamondDeployer{
+
+    Test1Facet test1Facet;
+
+    constructor() {
+        //deploy Test1Facet
+        test1Facet = new Test1Facet();
+
+        // get functions selectors but remove first element (supportsInterface)
+        bytes4[] memory fromGenSelectors  = removeIndex(0, generateSelectors("Test1Facet"));
+
+        // array of functions to add
+        FacetCut[] memory cutTest1 = new FacetCut[](1);
+        cutTest1[0] =
+        FacetCut({
+        facetAddress: address(test1Facet),
+        action: FacetCutAction.Add,
+        functionSelectors: fromGenSelectors
+        });
+
+        // add functions to diamond
+        ICut.diamondCut(cutTest1, address(0x0), "");
+
+    }
+
+    function test4AddTest1FacetFunctions() public {
+
+        // check if functions added to diamond
+        bytes4[] memory fromLoupeFacet = ILoupe.facetFunctionSelectors(address(test1Facet));
+        bytes4[] memory fromGenSelectors  = removeIndex(0, generateSelectors("Test1Facet"));
+        assertEq(abi.encode(fromLoupeFacet), abi.encode(fromGenSelectors));
+
+    }
+
+
+    function test5CanCallTest1FacetFunction() public {
+
+         // try to call function on new Facet
+        Test1Facet(address(diamond)).test1Func10();
+    }
+
+    function test6ReplaceSupportsInterfaceFunction() public {
+
+        // get supportsInterface selector from positon 0
+        bytes4[] memory fromGenSelectors =  new bytes4[](1);
+        fromGenSelectors[0] = generateSelectors("Test1Facet")[0];
+
+        // struct to replace function
+        FacetCut[] memory cutTest1 = new FacetCut[](1);
+        cutTest1[0] =
+        FacetCut({
+        facetAddress: address(test1Facet),
+        action: FacetCutAction.Replace,
+        functionSelectors: fromGenSelectors
+        });
+
+        // replace function by function on Test1 facet
+        ICut.diamondCut(cutTest1, address(0x0), "");
+
+        // check supportsInterface method connected to test1Facet
+        assertEq(address(test1Facet), ILoupe.facetAddress(fromGenSelectors[0]));
+
+    }
+
 }
